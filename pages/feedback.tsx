4c791e141bb51e2dc0b89d11502a7ddcb1d01939
minseung-gang <strause1@nate.com>
2023-08-18 15:18:39 +0900

    Feat: 피드백 페이지 추가

---
+++ pages/feedback.tsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+import Header from '../components/common/HeaderComponent';
+export default function feedback() {
+  return (
+    <Fragment>
+      <Header />
+      <main></main>
+    </Fragment>
+  );
+}
