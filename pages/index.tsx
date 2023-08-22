2b96ba01da49d8c833a8bb2bc44c3cf7b31d0562
minseung-gang <strause1@nate.com>
2023-08-22 23:30:00 +0900

    Fix: 데이터 페칭 및 getStaticProps 함수 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import HeaderButton from '../components/common/HeaderButton';
+import { useEffect } from 'react';
 import { Fragment } from 'react';
 import Header from '../components/common/HeaderComponent';
 import MapSection from '../components/home/MapSection';
+import { Store } from '../types/store';
+import { NextPage } from 'next';
+import useStores from '../hooks/useStores';
 
-export default function Home() {
+interface HomeProps {
+  stores: Store[];
+}
+
+const Home: NextPage<HomeProps> = ({ stores }) => {
+  const initializeStores = useStores();
+  console.log('store', stores);
+  useEffect(() => {
+    initializeStores(stores);
+  }, [initializeStores, stores]);
   return (
     <Fragment>
       <Header rightElements={<HeaderButton />} />
@@ -12,4 +25,15 @@ export default function Home() {
       </main>
     </Fragment>
   );
+};
+
+export default Home;
+
+export async function getStaticProps() {
+  const stores = (await import('../public/store.json')).default;
+
+  return {
+    props: { stores },
+    revalidate: 60 * 60,
+  };
 }
