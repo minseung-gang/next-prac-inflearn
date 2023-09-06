963b3383ea0cf6ee5f10f93eb5c0cca52a7b06c9
minseung-gang <strause1@nate.com>
2023-09-06 23:48:29 +0900

    Add: useCurrentStore 커스텀 훅 호출 및 onClick함수 props전달

--- components/home/Markers.tsx
+++ components/home/Markers.tsx
@@ -1,18 +1,22 @@
-import { MAP_KEY } from '@/hooks/useMaps';
-import { STORE_KEY } from '@/hooks/useStores';
-import { NaverMap } from '@/types/map';
-import { Store } from '@/types/store';
 import React from 'react';
 import useSWR from 'swr';
+import { MAP_KEY } from '../../hooks/useMaps';
+import { STORE_KEY } from '../../hooks/useStores';
+import useCurrentStore, {
+  CURRENT_STORE_KEY,
+} from '../../hooks/useCurrentStore';
+import type { ImageIcon, NaverMap } from '../../types/map';
+import type { Store } from '../../types/store';
 import Marker from './Marker';
 
 const Markers = () => {
-  //  const { data, error, isValidating, revalidate } = useSWR(key, fetcher);
   const { data: map } = useSWR<NaverMap>(MAP_KEY);
   const { data: stores } = useSWR<Store[]>(STORE_KEY);
 
-  if (!map || !stores) return null;
+  const { data: currentStore } = useSWR<Store>(CURRENT_STORE_KEY);
+  const { setCurrentStore, clearCurrentStore } = useCurrentStore();
 
+  if (!map || !stores) return null;
   return (
     <>
       {stores.map((store) => {
@@ -20,33 +24,48 @@ const Markers = () => {
           <Marker
             map={map}
             coordinates={store.coordinates}
+            icon={generateStoreMarkerIcon(store.season, false)}
+            onClick={() => {
+              setCurrentStore(store);
+            }}
             key={store.nid}
-            icon={storeMarkerIcon(store.season)}
           />
         );
       })}
+      {currentStore && (
+        <Marker
+          map={map}
+          coordinates={currentStore.coordinates}
+          icon={generateStoreMarkerIcon(currentStore.season, true)}
+          onClick={clearCurrentStore}
+          key={currentStore.nid}
+        />
+      )}
     </>
   );
 };
-
 export default Markers;
 
 const MARKER_HEIGHT = 64;
 const MARKER_WIDTH = 54;
 const NUMBER_OF_MARKER = 13;
 const SCALE = 2 / 3;
+
 const SCALED_MARKER_WIDTH = MARKER_WIDTH * SCALE;
 const SCALED_MARKER_HEIGHT = MARKER_HEIGHT * SCALE;
-const ICON_SPRITE_IMAGE_URL = '/images/markers.png';
 
-export function storeMarkerIcon(markerIndex: number) {
+export function generateStoreMarkerIcon(
+  markerIndex: number,
+  isSelected: boolean
+): ImageIcon {
+  /** https://navermaps.github.io/maps.js.ncp/docs/tutorial-8-marker-retina-sprite.example.html */
   return {
-    url: ICON_SPRITE_IMAGE_URL,
-    size: new naver.maps.Size(SCALED_MARKER_WIDTH, SCALED_MARKER_HEIGHT), // 이미지 크기
-    origin: new naver.maps.Point(SCALED_MARKER_WIDTH * markerIndex, 0), // 스프라이트 이미지에서 클리핑 위치
+    url: isSelected ? 'images/markers-selected.png' : 'images/markers.png',
+    size: new naver.maps.Size(SCALED_MARKER_WIDTH, SCALED_MARKER_HEIGHT),
+    origin: new naver.maps.Point(SCALED_MARKER_WIDTH * markerIndex, 0),
     scaledSize: new naver.maps.Size(
-      MARKER_WIDTH * NUMBER_OF_MARKER,
-      MARKER_HEIGHT
+      SCALED_MARKER_WIDTH * NUMBER_OF_MARKER,
+      SCALED_MARKER_HEIGHT
     ),
   };
 }
