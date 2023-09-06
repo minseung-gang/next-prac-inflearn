b40c100af1f930ed270e2ba8b5484cc9290b671b
minseung-gang <strause1@nate.com>
2023-09-06 23:50:02 +0900

    Add: 마커 타입에 icon 타입 추가

--- types/map.ts
+++ types/map.ts
@@ -1,15 +1,15 @@
-import { Coordinates } from './store';
+import type { Coordinates } from './store';
 
 export type NaverMap = naver.maps.Map;
 
 export type Marker = {
   map: NaverMap;
   coordinates: Coordinates;
-  onClick?: () => void;
   icon: ImageIcon;
+  onClick?: () => void;
 };
 
-type ImageIcon = {
+export type ImageIcon = {
   url: string;
   size: naver.maps.Size;
   origin: naver.maps.Point;
