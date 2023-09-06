bcf94bb3e4dc377d91c6216719797c9bc7798bb7
minseung-gang <strause1@nate.com>
2023-09-06 23:26:31 +0900

    Add: clearCurrentStore 커스텀 훅 호출 및 클릭이벤트에 적용

--- components/home/MapSection.tsx
+++ components/home/MapSection.tsx
@@ -1,13 +1,15 @@
 import Map from './Map';
 import Markers from './Markers';
 import useMap from '@/hooks/useMaps';
-
 import type { NaverMap } from '../../types/map';
+import useCurrentStore from '@/hooks/useCurrentStore';
 
 const MapSection = () => {
   const { initializeMap } = useMap();
+  const { clearCurrentStore } = useCurrentStore();
   const onLoadMap = (map: NaverMap) => {
     initializeMap(map);
+    naver.maps.Event.addListener(map, 'click', clearCurrentStore);
   };
 
   return (
