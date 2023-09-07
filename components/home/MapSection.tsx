e79cb0156e557bae62d50e250f8a6fce5f6597da
minseung-gang <strause1@nate.com>
2023-09-07 22:07:41 +0900

    Add: url 파라미터 값 가져와서 props로 전달하는 기능 추가

--- components/home/MapSection.tsx
+++ components/home/MapSection.tsx
@@ -1,10 +1,28 @@
 import Map from './Map';
 import Markers from './Markers';
-import useMap from '@/hooks/useMaps';
+import useMap, { INITIAL_CENTER, INITIAL_ZOOM } from '@/hooks/useMaps';
 import type { NaverMap } from '../../types/map';
 import useCurrentStore from '@/hooks/useCurrentStore';
+import { Coordinates } from '@/types/store';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
 
 const MapSection = () => {
+  const router = useRouter();
+  const query = useMemo(() => new URLSearchParams(router.asPath.slice(1)), []);
+  console.log(query, 'query');
+  const initialCenter = useMemo<Coordinates>(
+    () =>
+      query.get('lat') && query.get('lng')
+        ? [Number(query.get('lat')), Number(query.get('lng'))]
+        : INITIAL_CENTER,
+    [query]
+  );
+  const initialZoom = useMemo(
+    () => (query.get('zoom') ? Number(query.get('zoom')) : INITIAL_ZOOM),
+    [query]
+  );
+
   const { initializeMap } = useMap();
   const { clearCurrentStore } = useCurrentStore();
   const onLoadMap = (map: NaverMap) => {
@@ -14,7 +32,11 @@ const MapSection = () => {
 
   return (
     <>
-      <Map onLoad={onLoadMap} />
+      <Map
+        onLoad={onLoadMap}
+        initialCenter={initialCenter}
+        initialZoom={initialZoom}
+      />
       <Markers />
     </>
   );
