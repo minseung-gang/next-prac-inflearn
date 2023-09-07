8d943a59b02326d680fad7930e63f361d0a7626c
minseung-gang <strause1@nate.com>
2023-09-07 22:04:34 +0900

    FIx: props로 받은 onClickLogo함수 적용 타입 추가

---
+++ components/common/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../../styles/header.module.scss';
+
+interface Props {
+  onClickLogo?: () => void;
+  rightElements?: React.ReactElement[];
+}
+
+const HeaderComponent = ({ onClickLogo, rightElements }: Props) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.flexItem}>
+        <Link href="/" onClick={onClickLogo} className={styles.box}>
+          <Image
+            src="/inflearn.png"
+            width={110}
+            height={20}
+            alt="인프런 로고"
+          />
+        </Link>
+      </div>
+      {rightElements && <div className={styles.flexItem}>{rightElements}</div>}
+    </header>
+  );
+};
+
+export default HeaderComponent;
