116887e61885dee7f48161b67591fa562a759937
minseung-gang <strause1@nate.com>
2023-09-13 23:10:22 +0900

    Add: 클릭된 마커의 store를 가져와 적용

--- components/home/DetailSection.tsx
+++ components/home/DetailSection.tsx
@@ -1,14 +1,20 @@
 import styles from '../../styles/detail.module.scss';
 import { IoIosArrowUp } from 'react-icons/io';
+import { CURRENT_STORE_KEY } from '@/hooks/useCurrentStore';
+import useSWR from 'swr';
+import { Store } from '@/types/store';
 
 const DetailSection = () => {
+  const { data: currentStore } = useSWR<Store>(CURRENT_STORE_KEY);
   return (
     <div className={styles.detailSection}>
       <div className={styles.header}>
         <button className={styles.arrowButton} disabled>
           <IoIosArrowUp size={20} color="#666666" />
         </button>
-        <p className={styles.title}>매장을 선택해주세요</p>
+
+        {!currentStore && <p className={styles.title}>매장을 선택하세요</p>}
+        {currentStore && <p className={styles.title}>{currentStore.name}</p>}
       </div>
     </div>
   );
