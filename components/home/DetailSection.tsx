bb9d8bd7fc70daf72d4597850e0442b6588be311
minseung-gang <strause1@nate.com>
2023-10-03 19:37:51 +0900

    Add: 클릭 했을 때와 클릭 하지 않았을 때에 맞춰 css적용

--- components/home/DetailSection.tsx
+++ components/home/DetailSection.tsx
@@ -1,21 +1,36 @@
 import styles from '../../styles/detail.module.scss';
+import { useState } from 'react';
 import { IoIosArrowUp } from 'react-icons/io';
 import { CURRENT_STORE_KEY } from '@/hooks/useCurrentStore';
 import useSWR from 'swr';
 import { Store } from '@/types/store';
+import DetailContent from './DetailContent';
 
 const DetailSection = () => {
   const { data: currentStore } = useSWR<Store>(CURRENT_STORE_KEY);
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className={styles.detailSection}>
+    <div
+      className={`${styles.detailSection} 
+      ${currentStore ? styles.selected : ''}
+      ${expanded ? styles.expanded : ''}`}
+    >
       <div className={styles.header}>
-        <button className={styles.arrowButton} disabled>
+        <button
+          className={`${styles.arrowButton} ${expanded ? styles.expanded : ''}`}
+          onClick={() => setExpanded(!expanded)}
+          disabled={!currentStore}
+        >
           <IoIosArrowUp size={20} color="#666666" />
         </button>
 
-        {!currentStore && <p className={styles.title}>매장을 선택하세요</p>}
-        {currentStore && <p className={styles.title}>{currentStore.name}</p>}
+        {currentStore ? (
+          <p className={styles.title}>{currentStore.name}</p>
+        ) : (
+          <p className={styles.title}>매장을 선택하세요</p>
+        )}
       </div>
+      <DetailContent currentStore={currentStore} expanded={expanded} />
     </div>
   );
 };
