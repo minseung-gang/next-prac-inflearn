8e76d411197126f2c624f7f974775b27761734e3
minseung-gang <strause1@nate.com>
2023-10-04 22:06:04 +0900

    Fix: Header부분 컴포넌트화 및 스타일 수정

--- components/home/DetailSection.tsx
+++ components/home/DetailSection.tsx
@@ -1,35 +1,26 @@
-import styles from '../../styles/detail.module.scss';
 import { useState } from 'react';
-import { IoIosArrowUp } from 'react-icons/io';
-import { CURRENT_STORE_KEY } from '@/hooks/useCurrentStore';
 import useSWR from 'swr';
-import { Store } from '@/types/store';
+import { CURRENT_STORE_KEY } from '../../hooks/useCurrentStore';
+import type { Store } from '../../types/store';
+import styles from '../../styles/detail.module.scss';
+import DetailHeader from './DetailHeader';
 import DetailContent from './DetailContent';
 
 const DetailSection = () => {
   const { data: currentStore } = useSWR<Store>(CURRENT_STORE_KEY);
   const [expanded, setExpanded] = useState(false);
+
   return (
     <div
-      className={`${styles.detailSection} 
-      ${currentStore ? styles.selected : ''}
-      ${expanded ? styles.expanded : ''}`}
+      className={`${styles.detailSection} ${expanded ? styles.expanded : ''} ${
+        currentStore ? styles.selected : ''
+      }`}
     >
-      <div className={styles.header}>
-        <button
-          className={`${styles.arrowButton} ${expanded ? styles.expanded : ''}`}
-          onClick={() => setExpanded(!expanded)}
-          disabled={!currentStore}
-        >
-          <IoIosArrowUp size={20} color="#666666" />
-        </button>
-
-        {currentStore ? (
-          <p className={styles.title}>{currentStore.name}</p>
-        ) : (
-          <p className={styles.title}>매장을 선택하세요</p>
-        )}
-      </div>
+      <DetailHeader
+        currentStore={currentStore}
+        expanded={expanded}
+        onClickArrow={() => setExpanded(!expanded)}
+      />
       <DetailContent currentStore={currentStore} expanded={expanded} />
     </div>
   );
