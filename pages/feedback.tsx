213ab05362a45f689104ee840f005934bc027e70
minseung-gang <strause1@nate.com>
2023-10-05 19:44:42 +0900

    Add: NextSeo추가

--- pages/feedback.tsx
+++ pages/feedback.tsx
@@ -1,8 +1,15 @@
 import React, { Fragment } from 'react';
 import Header from '../components/common/Header';
+import { NextSeo } from 'next-seo';
+
 export default function feedback() {
   return (
     <Fragment>
+      <NextSeo
+        title="피드백"
+        description="매장 지도 서비스에 대한 피드백을 받습니다."
+        /* canonical={} */
+      />
       <Header />
       <main></main>
     </Fragment>
