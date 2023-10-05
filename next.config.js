319ee44c65c5b814e61ca56228a714cff0fc72c9
minseung-gang <strause1@nate.com>
2023-10-05 19:42:10 +0900

    Add: lang관련 국제화(i18n) 관련 속성 추가

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     domains: ['lecture-1.vercel.app', 'search.pstatic.net'],
   },
-}
+  i18n: {
+      /** 국제화 관련 속성 추가 https://nextjs.org/docs/advanced-features/i18n-routing#getting-started */
+    /** 사이트 언어의 후보를 배열로 나타냄 */
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
