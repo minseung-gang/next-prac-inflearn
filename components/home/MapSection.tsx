e2ad492831c0581b0a81d8dba36c725f422b6fe7
minseung-gang <strause1@nate.com>
2023-10-05 22:11:40 +0900

    Add: 파비콘 이미지 추가

--- components/home/MapSection.tsx
+++ components/home/MapSection.tsx
@@ -9,8 +9,9 @@ import { useMemo } from 'react';
 
 const MapSection = () => {
   const router = useRouter();
+
   const query = useMemo(() => new URLSearchParams(router.asPath.slice(1)), []);
-  console.log(query, 'query');
+
   const initialCenter = useMemo<Coordinates>(
     () =>
       query.get('lat') && query.get('lng')
