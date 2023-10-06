0de1c554ec325862b03bdd45d4163613d3fda836
minseung-gang <strause1@nate.com>
2023-10-06 22:19:07 +0900

    Add: 구글 애널리틱스 코드 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
 import '@/styles/globals.scss';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 import { DefaultSeo } from 'next-seo';
 import SEO from '../seo.config';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'GTM-5DB25ZFM');
+            `,
+          }}
+        />
+      </Head>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=GTM-5DB25ZFM`}
+      />
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
