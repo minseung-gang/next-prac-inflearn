70d60ec3d5aa55a3dde66375aca51be9e08858dd
minseung-gang <strause1@nate.com>
2023-10-09 22:33:16 +0900

    Fix: script태그를 next/script 코드로 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,19 +8,19 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-PGGY2X86EE');
-            `,
-          }}
-        />
-      </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-PMBX725CN0');
+          `,
+        }}
+      />
+
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=G-PGGY2X86EE`}
